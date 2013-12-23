7e9a5edc717b03539ea46c166f534ef5a4b3ab02
Jacky Chan <libing.chen@gmail.com>
2013-12-23 16:37:36 +0800

    add execute command

--- src/main/java/org/mvnsearch/zookeeper/shell/service/impl/ZooKeeperServiceImpl.java
+++ src/main/java/org/mvnsearch/zookeeper/shell/service/impl/ZooKeeperServiceImpl.java
@@ -1,5 +1,6 @@
 package org.mvnsearch.zookeeper.shell.service.impl;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
@@ -8,6 +9,7 @@ import org.mvnsearch.zookeeper.shell.service.ZooKeeperService;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PreDestroy;
+import java.net.Socket;
 
 /**
  * Zoo Keeper Service implementation
@@ -17,6 +19,7 @@ import javax.annotation.PreDestroy;
 @Component("zooKeeperService")
 public class ZooKeeperServiceImpl implements ZooKeeperService {
     private CuratorFramework curator;
+    private String server;
 
     @PreDestroy
     public void close() {
@@ -32,9 +35,21 @@ public class ZooKeeperServiceImpl implements ZooKeeperService {
         RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 1);
         this.curator = CuratorFrameworkFactory.newClient(server, retryPolicy);
         this.curator.start();
+        this.server = server;
     }
 
     public CuratorFramework getCurator() {
         return this.curator;
+    }
+
+    public String executeCommand(String command) throws Exception {
+        String[] parts = server.split(":");
+        Socket sock = new Socket(parts[0], Integer.valueOf(parts[1]));
+        IOUtils.write(command.getBytes(), sock.getOutputStream());
+        String content = IOUtils.toString(sock.getInputStream());
+        if (!sock.isClosed()) {
+            sock.close();
+        }
+        return content.trim();
     }
 }
