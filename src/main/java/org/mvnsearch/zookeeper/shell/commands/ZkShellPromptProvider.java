1767a6a3c5bab76998f5363090f3610e47b56df0
Jacky Chan <libing.chen@gmail.com>
2013-12-23 10:05:54 +0800

    ZooKeeper Shell prompt provider

---
+++ src/main/java/org/mvnsearch/zookeeper/shell/commands/ZkShellPromptProvider.java
@@ -0,0 +1,57 @@
+package org.mvnsearch.zookeeper.shell.commands;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.shell.plugin.support.DefaultPromptProvider;
+import org.springframework.shell.support.util.OsUtils;
+import org.springframework.stereotype.Component;
+
+/**
+ * ZooKeeper Shell prompt provider
+ *
+ * @author linux_china
+ */
+@Component
+public class ZkShellPromptProvider extends DefaultPromptProvider implements InitializingBean {
+    /**
+     * prompt
+     */
+    public static String prompt = "zk";
+    /**
+     * symbol
+     */
+    public static String symbol = "#";
+
+    /**
+     * init method
+     *
+     * @throws Exception exception
+     */
+    public void afterPropertiesSet() throws Exception {
+        //if Windows OS, adjust symbo to '>'
+        if ((OsUtils.isWindows())) {
+            symbol = ">";
+        }
+    }
+
+    /**
+     * prompt
+     *
+     * @return prompt
+     */
+    @Override
+    public String getPrompt() {
+        String currentPath = StringUtils.defaultIfEmpty(ZkShellOperationCommands.currentPath, "/");
+        return "[" + prompt + ":" + currentPath + "]" + symbol;
+    }
+
+    /**
+     * name
+     *
+     * @return name
+     */
+    @Override
+    public String name() {
+        return "zookeeper-shell-java-cli-prompt-provider";
+    }
+}
