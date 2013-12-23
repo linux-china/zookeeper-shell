552fd25d0ed7b31990b89f15d0f48ce79e3314e8
Jacky Chan <libing.chen@gmail.com>
2013-12-23 11:48:43 +0800

    ZooKeeperServiceImpl test case

---
+++ src/test/java/org/mvnsearch/zookeeper/shell/service/impl/ZooKeeperServiceImplTest.java
@@ -0,0 +1,29 @@
+package org.mvnsearch.zookeeper.shell.service.impl;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * ZooKeeperServiceImpl test case
+ *
+ * @author linux_china
+ */
+public class ZooKeeperServiceImplTest {
+    private static ZooKeeperServiceImpl zooKeeperService;
+
+    @BeforeClass
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testConnection() {
+
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+
+    }
+}
