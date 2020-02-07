7587ac37680a072e207c9b455c951376aa6aab51
linux_china <libing.chen@gmail.com>
2020-02-06 20:47:34 -0800

    Zookeeper shell App

---
+++ src/main/java/org/mvnsearch/zookeeper/shell/ZooKeeperShellApp.java
@@ -0,0 +1,16 @@
+package org.mvnsearch.zookeeper.shell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Zookeeper shell App
+ *
+ * @author linux_china
+ */
+@SpringBootApplication
+public class ZooKeeperShellApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ZooKeeperShellApp.class, args);
+    }
+}
