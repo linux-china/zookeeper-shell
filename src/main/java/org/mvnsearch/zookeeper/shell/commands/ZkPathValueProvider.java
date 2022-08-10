d6659585aa035a6ca8592890bac56770fb5fc896
linux_china <libing.chen@gmail.com>
2022-08-10 16:04:25 -0700

    chore: API adjustecd

--- src/main/java/org/mvnsearch/zookeeper/shell/commands/ZkPathValueProvider.java
+++ src/main/java/org/mvnsearch/zookeeper/shell/commands/ZkPathValueProvider.java
@@ -2,13 +2,11 @@ package org.mvnsearch.zookeeper.shell.commands;
 
 import org.mvnsearch.zookeeper.shell.service.ZooKeeperService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.MethodParameter;
 import org.springframework.shell.CompletionContext;
 import org.springframework.shell.CompletionProposal;
 import org.springframework.shell.standard.ValueProvider;
 import org.springframework.stereotype.Component;
 
-import java.lang.reflect.Method;
 import java.util.Collections;
 import java.util.List;
 
@@ -20,20 +18,17 @@ public class ZkPathValueProvider implements ValueProvider {
     private static final List<String> ALLOWED_METHODS = List.of("cd", "stat", "cat", "watch");
 
     @Override
-    public boolean supports(MethodParameter parameter, CompletionContext completionContext) {
-        final Method method = parameter.getMethod();
-        return method != null && method.getDeclaringClass() == ZkShellOperationCommands.class && ALLOWED_METHODS.contains(method.getName());
-    }
-
-    @Override
-    public List<CompletionProposal> complete(MethodParameter parameter, CompletionContext completionContext, String[] hints) {
-        String destPath = ZkShellOperationCommands.getAbsolutePath("");
-        try {
-            List<String> children = zooKeeperService.getCurator().getChildren().forPath(destPath);
-            if (!children.isEmpty()) {
-                return children.stream().map(CompletionProposal::new).toList();
+    public List<CompletionProposal> complete(CompletionContext completionContext) {
+        final String command = completionContext.getCommandRegistration().getCommand();
+        if (ALLOWED_METHODS.contains(command)) {
+            String destPath = ZkShellOperationCommands.getAbsolutePath("");
+            try {
+                List<String> children = zooKeeperService.getCurator().getChildren().forPath(destPath);
+                if (!children.isEmpty()) {
+                    return children.stream().map(CompletionProposal::new).toList();
+                }
+            } catch (Exception ignore) {
             }
-        } catch (Exception ignore) {
         }
         return Collections.emptyList();
     }
